5e3f650575e6841a4c7dd9355f0a061c963a9993
Matt Dryden <48354684+mattdryden@users.noreply.github.com>
2019-02-13 17:13:03 +0000

    Initial

---
+++ src/app/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './main.css';
+
+import Wadoclock from '../wadoclock';
+
+class App extends Component {
+  render() {
+    return (<Wadoclock />);
+  }
+}
+
+export default App;
---
+++ src/wadoclock/main.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './main.css';
+
+class Wadoclock extends Component {
+    render() {
+        return (<div>What's the timer Mr Wadders?</div>);
+    }
+}
+
+export default Wadoclock;
