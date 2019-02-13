5c11ad2e405de025debac4b318a217b523c7ffc8
Matt Dryden <48354684+mattdryden@users.noreply.github.com>
2019-02-13 18:08:22 +0000

    Front page works

--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './main.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Wadoclock from '../wadoclock';
 
 class App extends Component {
   render() {
-    return (<Wadoclock />);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => <Wadoclock />} />
+          <Route exact path="/:event/:date" render={(props) => <Wadoclock event={props.match.params.event} date={props.match.params.date} />} />
+        </Switch>
+      </Router>
+        );
   }
 }
 
--- src/wadoclock/main.js
+++ src/wadoclock/main.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import Wado from '../wado';
+
 import './main.css';
 
 class Wadoclock extends Component {
+
+    renderWado() {
+        return <Wado />
+    }
+
     render() {
-        return (<div>What's the timer Mr Wadders?</div>);
+        let { event, date } = this.props;
+
+        if(typeof event === 'undefined' && typeof date === 'undefined') {
+            return this.renderWado();
+        }
+        else {
+            return (<div>What's the timer Mr Wadders?</div>);
+        }
     }
 }
 
